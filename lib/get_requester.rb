cb2f6c6d35332b3b3bdb598ac73ae803766978ce
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-05-29 13:26:40 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -2,6 +2,7 @@ require 'net/http'
 #require 'open-url'
 require 'json'
 
+<<<<<<< HEAD
 
 class GetRequester
   attr_reader :url 
@@ -9,6 +10,11 @@ class GetRequester
     @url = url
   #url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
 end 
+=======
+class GetRequester
+  
+  URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+>>>>>>> 9502157b8dd604c7b102edc8cc7c07458d865027
   
   def get_response_body
     uri = URI.parse(URL)
@@ -19,11 +25,20 @@ end
   def parse_json
     requester = JSON.parse(self.get_response_body)
     requester.collect do |requester|
+<<<<<<< HEAD
     requester
       
+=======
+      requester["people"]
+>>>>>>> 9502157b8dd604c7b102edc8cc7c07458d865027
   end 
 end 
 end 
 
+<<<<<<< HEAD
 
+=======
+#Requester = GetRequester.new.get_requester
+#puts requester 
+>>>>>>> 9502157b8dd604c7b102edc8cc7c07458d865027
 
